96b7f211e59223cbd9173db639d08b1e828a6736
BIGRED <RED@Jerods-MBP.attlocal.net>
2016-04-24 16:57:45 -0500

    step3 step4 complete     just need to add css to exercise for pazazz

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,14 +1,53 @@
 <?php
 
+ $adjectives = ['tickled','obnoxious','bumpy','whispering','purring','wasteful','immense','jittery', 'outrageous','husky'];
 
-$adjectives [
-    addicting, 
-       
-]
+$nouns = ['ferret','monkey','taint','mortician','weiner','cooter','tuber','wombat','airhead','jerk'];
 
-$nouns [
-    
-]
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Name Generator</title>
+</head>
+<body>
+    <h1>Server Name Gererator</h1>
+     <p>
+        <?php 
+            function adjectives_random($adjectives, $num = 1) {
+            shuffle($adjectives);
+        
+                $adjective = [];
+                for ($i = 0; $i < $num; $i++) {
+                    $adjective[] = $adjectives[$i];
+                }
+                return $num == 1 ? $adjective[0] : $adjective;
+            }
+
+            function nouns_random($nouns, $num = 1) {
+            shuffle($nouns);
+        
+                $noun = [];
+                for ($i = 0; $i < $num; $i++) {
+                    $noun[] = $nouns[$i];
+                }
+                return $num == 1 ? $noun[0] : $noun;
+            }
+                echo adjectives_random($adjectives) . "-" . nouns_random($nouns);
+        ?>
+    </p>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
 
 
 
