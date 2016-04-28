28ee7455f1c3191b83164621ea4b9677b4d63afc
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-04-28 15:03:59 -0500

    complete exercise INCLUDE REQUIRE

--- public/ping.php
+++ public/ping.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'functions/functions.php';
+
 function pageController() 
 {
 
@@ -8,7 +10,7 @@ function pageController()
     //     $count = $_GET['count'];
     // }  **** this is the same as the statement below *****
 
-    $count = !isset($_GET['count']) ? 0 : $_GET['count'];
+    $count = inputGet('count');
     return ['count' => $count];
 
 }
@@ -36,9 +38,15 @@ extract(pageController());
             color: white;
             font-size: 30px;
         }
-
-        
-
+        .ball {
+            position:fixed;
+        }
+        #miss {
+            margin-left: 800px;
+        }
+        p {
+            margin-left: 600px;
+        }
         </style>
 
 </head>
@@ -46,14 +54,60 @@ extract(pageController());
 <body>
         
         <p> <?= $count ?> </p>
-        <a href='pong.php?count=<?= $count+1?>'><img src="img/pingpongball.png" width="75" height="75"></a>
-        <a href='pong.php?count=<?= $count=0?>'>MISS</a>
+        <a href='pong.php?count=0'>MISS</a>
+        <div class="ball"><a href='pong.php?count=<?= $count+1?>'><img src="img/pingpongball.png" width="75" height="75"></a></div>
+        
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 <script type="text/javascript">
+
+$(document).ready(function(){
+    animateDiv();
+    
+});
+
+function makeNewPosition(){
+    
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
+    
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+    
+    return [nh,nw];    
+    
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    var oldq = $('.ball').offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+    
+    $('.ball').animate({ top: newq[0], left: newq[1] }, speed, function(){
+      animateDiv();        
+    });
+    
+};
+
+function calcSpeed(prev, next) {
+    
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+    
+    var greatest = x > y ? x : y;
+    
+    var speedModifier = 0.05;
+
+    var speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+
+}
+
 
 </script>
 
