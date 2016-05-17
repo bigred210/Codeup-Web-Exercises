e46bce225113c46453f40a42e038663872d8cd37
BIGRED <RED@Jerods-MacBook-Pro.local>
2016-05-17 14:02:11 -0500

    steps 1-4 complete

--- public/rectangle.php
+++ public/rectangle.php
@@ -3,13 +3,33 @@
 
 class Rectangle
 {
-    public $length;
-    public $height;
+    private $length;
+    private $height;
 
     public function __construct($height,$length)
+    { 
+        $this->setLength($length);
+        $this->setHeight($height);
+    }
+
+    public function setHeight($height)
     {
-        $this->length = $length;
-        $this->height  = $height;
+        $this->height = trim($height);
+    }
+
+    public function setLength($length)
+    {
+        $this->length = trim($length);
+    }
+
+    public function getHeight()
+    {
+        return $this->height;
+    }
+
+    public function getLength()
+    {
+        return $this->length;
     }
 
     public function area()
